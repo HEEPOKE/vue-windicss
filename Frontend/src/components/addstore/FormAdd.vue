e45a0097d58065fa13804c2b3478819e90cd8f06
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-02 17:19:10 +0700

    Update FormAdd.vue

--- Frontend/src/components/addstore/FormAdd.vue
+++ Frontend/src/components/addstore/FormAdd.vue
@@ -1,63 +1,65 @@
 <template>
   <HeaderVue />
   <div class="relative grid place-items-center">
-    <div class="w-10/12 bg-gray-800 rounded-lg mt-3">
+    <div
+      class="w-10/12 rounded-lg border mt-3 border-dark-700 bg-light-100 dark:bg-dark-800 dark:border-gray-500"
+    >
       <form>
         <div class="ml-3 mr-3 mt-2">
-          <label class="block mb-2 font-extrabold text-lg text-white"
-            >name</label
+          <label class="block mb-2 font-extrabold text-lg dark:text-white"
+            >Name</label
           >
           <input
             type="text"
             name="name"
-            class="text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+            class="text-md rounded-lg block w-full p-2.5 border border-gray-200 bg-gray-300 text-dark-900 placeholder-dark-200 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             placeholder="Enter Name"
             required
           />
         </div>
         <div class="ml-3 mr-3 mt-2">
-          <label class="block mb-2 font-extrabold text-lg text-white">
-            color
+          <label class="block mb-2 font-extrabold text-lg dark:text-white">
+            Color
           </label>
           <input
             type="text"
             name="color"
-            class="text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+            class="text-md rounded-lg block w-full p-2.5 border border-gray-200 bg-gray-300 text-dark-900 placeholder-dark-200 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             placeholder="Enter Color"
             required
           />
         </div>
         <div class="ml-3 mr-3 mt-2">
-          <label class="block mb-2 font-extrabold text-lg text-white">
-            category
+          <label class="block mb-2 font-extrabold text-lg dark:text-white">
+            Category
           </label>
           <input
             type="text"
             name="category"
-            class="text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+            class="text-md rounded-lg block w-full p-2.5 border border-gray-200 bg-gray-300 text-dark-900 placeholder-dark-200 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             placeholder="Enter Category"
             required
           />
         </div>
         <div class="ml-3 mr-3 mt-2">
-          <label class="block mb-2 font-extrabold text-lg text-white">
-            preice
+          <label class="block mb-2 font-extrabold text-lg dark:text-white">
+            Price
           </label>
           <input
             type="number"
             name="price"
-            class="text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+            class="text-md rounded-lg block w-full p-2.5 border border-gray-200 bg-gray-300 text-dark-900 placeholder-dark-200 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             placeholder="Enter Price"
             min="{0}"
             required
           />
         </div>
         <div class="ml-3 mr-3 mt-2">
-          <label class="block mb-2 font-extrabold text-lg text-white">
-            image
+          <label class="block mb-2 font-extrabold text-lg dark:text-white">
+            Image
           </label>
           <input
-            class="block mb-5 w-full text-sm rounded-lg border cursor-pointer text-gray-400 focus:outline-none bg-gray-600 border-gray-500 placeholder-gray-400"
+            class="block mb-5 w-full text-md rounded-lg border cursor-pointer focus:outline-none border-gray-200 bg-gray-300 text-dark-900 placeholder-dark-200 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-gray-400"
             type="file"
             name="image"
             accept="image/*"
@@ -67,7 +69,7 @@
         <div class="flex justify-center">
           <button
             type="submit"
-            class="text-white w-1/3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+            class="text-white w-1/3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2"
           >
             Save
           </button>
@@ -76,7 +78,7 @@
       <div class="flex justify-center">
         <button
           type="button"
-          class="text-white w-1/3 bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+          class="text-white w-1/3 bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2"
           @click="Back"
         >
           Back
