0daba070fa783b8ca0709ae3734c0c02f486fb6e
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-02 17:35:39 +0700

    add glass

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,14 +1,19 @@
 import { createApp } from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faBars, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faMoon,
+  faSun,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 import App from "./App.vue";
 import router from "./routes/routes";
 import "virtual:windi.css";
 import "./assets/css/Navbar.css";
 import "./assets/css/main.css";
 
-library.add(faBars, faMoon, faSun);
+library.add(faBars, faMoon, faSun, faMagnifyingGlass);
 
 createApp(App)
   .component("font-awesome-icon", FontAwesomeIcon)
