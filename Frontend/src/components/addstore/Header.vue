5283c5b973ef2556f4319c756122b1b6bc5f9469
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-02 16:56:01 +0700

    Update Header.vue

--- Frontend/src/components/addstore/Header.vue
+++ Frontend/src/components/addstore/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="text-3xl font-bold text-dark-900 text-center hover:text-purple-500 dark:text-zinc-500 dark:hover:text-purple-700 mt-3"
+    class="text-3xl font-bold text-dark-900 text-center cursor-pointer hover:text-purple-500 dark:text-zinc-500 dark:hover:text-purple-700 mt-3"
+    @click="Add"
   >
     ADD
   </div>
@@ -8,5 +9,10 @@
 <script lang="ts">
 export default {
   name: "HeaderVue",
+  methods: {
+    Add() {
+      window.location.reload();
+    },
+  },
 };
 </script>
