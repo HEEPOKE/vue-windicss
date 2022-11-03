e271dee814842b88d6afc60057394c6c47c3c2c0
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-03 09:09:27 +0700

    Update Table.vue

--- Frontend/src/components/store/Table.vue
+++ Frontend/src/components/store/Table.vue
@@ -15,28 +15,28 @@
     </div>
     <table class="w-full text-sm text-left text-gray-400">
       <thead class="text-xs uppercase bg-gray-700 text-gray-400">
-        <tr class="text-center">
-          <th scope="col" class="p-4 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-900">#</th>
-          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-900">Product name</th>
-          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-900">Color</th>
-          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-900">Category</th>
-          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-900">Price</th>
-          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-900">Action</th>
+        <tr class="text-center ">
+          <th scope="col" class="p-4 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">#</th>
+          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Product name</th>
+          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Color</th>
+          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Category</th>
+          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Price</th>
+          <th scope="col" class="py-3 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Action</th>
         </tr>
       </thead>
       <tbody class="text-center">
-        <tr class="border-b bg-gray-800 border-gray-700 hover:bg-gray-600">
-          <td class="p-4 w-4">1</td>
+        <tr class="border-b bg-gray-800 border-gray-700 hover:bg-dark-300">
+          <td class="p-4 w-4 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">1</td>
           <th
             scope="row"
-            class="py-4 px-6 font-medium whitespace-nowrap text-white"
+            class="py-4 px-6 font-medium whitespace-nowrap text-white dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
           >
             Apple MacBook Pro 17
           </th>
-          <td class="py-4 px-6">Sliver</td>
-          <td class="py-4 px-6">Laptop</td>
-          <td class="py-4 px-6">$2999</td>
-          <td class="py-4 px-6">
+          <td class="py-4 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Sliver</td>
+          <td class="py-4 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Laptop</td>
+          <td class="py-4 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">$2999</td>
+          <td class="py-4 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">
             <button
               type="button"
               class="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
@@ -51,18 +51,18 @@
             </button>
           </td>
         </tr>
-        <tr class="border-b bg-gray-800 border-gray-700 hover:bg-gray-600">
-          <td class="p-4 w-4">2</td>
+        <tr class="border-b bg-gray-800 border-gray-700 hover:bg-dark-300">
+          <td class="p-4 w-4 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">2</td>
           <th
             scope="row"
-            class="py-4 px-6 font-medium whitespace-nowrap text-white"
+            class="py-4 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300 font-medium whitespace-nowrap text-white"
           >
             Microsoft Surface Pro
           </th>
-          <td class="py-4 px-6">White</td>
-          <td class="py-4 px-6">Laptop PC</td>
-          <td class="py-4 px-6">$1999</td>
-          <td class="py-4 px-6">
+          <td class="py-4 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">White</td>
+          <td class="py-4 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Laptop PC</td>
+          <td class="py-4 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">$1999</td>
+          <td class="py-4 px-6 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">
             <button
               type="button"
               class="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
