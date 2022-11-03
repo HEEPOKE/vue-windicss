3b44489619c99baf7ec608a88a3c9ac2fed97205
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-03 09:41:56 +0700

    heaer

---
+++ Frontend/src/components/store/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div
+      class="text-3xl font-bold text-dark-900 text-center cursor-pointer hover:text-purple-500 dark:text-zinc-500 dark:hover:text-purple-700 mt-3"
+      @click="Store"
+    >
+    STORE
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "HeaderVue",
+  methods: {
+    Store() {
+      window.location.reload();
+    },
+  },
+};
+</script>
--- Frontend/src/components/store/Table.vue
+++ Frontend/src/components/store/Table.vue
@@ -1,9 +1,15 @@
 <template>
+  <HeaderVue />
   <div class="overflow-x-auto relative shadow-md mt-6">
-    <div class="flex items-center pb-4 mt-1 ml-2 lg:justify-end mr-3 md:justify-center">
+    <div
+      class="flex items-center pb-4 mt-1 ml-2 lg:justify-end mr-3 md:justify-center"
+    >
       <div class="relative">
         <div class="flex absolute inset-y-0 items-center">
-          <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="ml-5 text-dark-900 dark:text-gray-400" />
+          <font-awesome-icon
+            icon="fa-solid fa-magnifying-glass"
+            class="ml-5 text-dark-900 dark:text-gray-400"
+          />
         </div>
         <input
           type="text"
@@ -16,26 +22,74 @@
     <table class="w-full text-sm table-auto">
       <thead>
         <tr class="text-center">
-          <th scope="col" class="p-4 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">#</th>
-          <th scope="col" class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Product name</th>
-          <th scope="col" class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Color</th>
-          <th scope="col" class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Category</th>
-          <th scope="col" class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Price</th>
-          <th scope="col" class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Action</th>
+          <th
+            scope="col"
+            class="p-4 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            #
+          </th>
+          <th
+            scope="col"
+            class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            Product name
+          </th>
+          <th
+            scope="col"
+            class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            Color
+          </th>
+          <th
+            scope="col"
+            class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            Category
+          </th>
+          <th
+            scope="col"
+            class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            Price
+          </th>
+          <th
+            scope="col"
+            class="py-3 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            Action
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr class="text-center">
-          <td class="p-4 w-4 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">1</td>
+          <td
+            class="p-4 w-4 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            1
+          </td>
           <td
             class="py-4 px-6 border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
           >
             Apple MacBook Pro 17
           </td>
-          <td class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Sliver</td>
-          <td class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Laptop</td>
-          <td class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">$2999</td>
-          <td class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">
+          <td
+            class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            Sliver
+          </td>
+          <td
+            class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            Laptop
+          </td>
+          <td
+            class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            $2999
+          </td>
+          <td
+            class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
             <button
               type="button"
               class="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
@@ -51,16 +105,34 @@
           </td>
         </tr>
         <tr class="text-center">
-          <td class="p-4 w-4 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">2</td>
+          <td
+            class="p-4 w-4 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            2
+          </td>
           <td
             class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
           >
             Microsoft Surface Pro
           </td>
-          <td class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">White</td>
-          <td class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">Laptop PC</td>
-          <td class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">$1999</td>
-          <td class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300">
+          <td
+            class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            White
+          </td>
+          <td
+            class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            Laptop PC
+          </td>
+          <td
+            class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
+            $1999
+          </td>
+          <td
+            class="py-4 px-6 border border-dark-900 text-dark-900 bg-light-100 hover:bg-light-600 dark:border-light-100 dark:text-white dark:bg-dark-800 dark:hover:bg-dark-300"
+          >
             <button
               type="button"
               class="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
@@ -80,7 +152,12 @@
   </div>
 </template>
 <script lang="ts">
+import HeaderVue from "./Header.vue";
+
 export default {
   name: "TableVue",
+  components: {
+    HeaderVue,
+  },
 };
 </script>
