a33d14f869e294f64b538d4a7ff62976d284fe09
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-03 11:25:48 +0700

    Update App.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="App">
+    <widget-container-modal />
     <router-view />
   </div>
 </template>
+<script lang="ts">
+import { container } from "jenesius-vue-modal";
+
+export default {
+  name: "App",
+  components: { WidgetContainerModal: container },
+};
+</script>
