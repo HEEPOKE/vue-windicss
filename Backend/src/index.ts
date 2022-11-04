06802b488194eb9c61a341a9dad72f4194edf9de
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-04 15:28:38 +0700

    set route

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,7 +6,7 @@ import helmet from "helmet";
 import { errorHandler } from "./middleware/error.middleware";
 import { notFoundHandler } from "./middleware/notfound.middleware";
 // import config from "./config";
-import api from "./routes/routes";
+import router from "./routes/routes";
 
 dotenv.config();
 
@@ -18,9 +18,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-app.use(helmet());
+// app.use(helmet());
 app.use(errorHandler);
 app.use(notFoundHandler);
 
-app.use("/api", api.routes);
+app.use("/api", router);
 app.listen(port, () => console.log("https://localhost:" + port || 6476));
